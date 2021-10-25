4fcac84bdfdda554aba072bf8e61bd48521a4102
yujin-gu <93050659+yujin-gu@users.noreply.github.com>
2021-10-25 17:49:34 -0400

    First commit
    First commit

---
+++ Hello的副本.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+public class Hello {
+
+    public static void main(String[] args) {
+        System.out.print("Hello. What is your name? ");
+        Scanner sc = new Scanner(System.in);
+        String input = sc.next();
+        //ask the user to input his/her name
+        System.out.print("It's nice to meet you," + input + ". How old are you? ");
+
+        String age = sc.next();
+        //ask the user to input his/her age
+        System.out.println("I see that you are still quite young at only " + age + ".");
+
+        System.out.print("Where do you live? ");
+        String location = sc.next();
+        //ask the user for his/her location
+        System.out.println("Wow! I've always wanted to go to " + location + "." + " Thanks for chatting with me. Bye! ");
+    }
+}
